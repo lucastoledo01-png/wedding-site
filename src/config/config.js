8a5f862024bdbbfa5d71cd4d802f3ee24221f78f
Lucas Toledo <lucastoledoup@gmail.com>
2026-07-14 23:10:35 -0300

    fix(audio): update fallback soundcloud url to a valid track

--- src/config/config.js
+++ src/config/config.js
@@ -67,7 +67,7 @@ const config = {
       // Music file (choose one or replace with your own file)
       src: "",
       soundcloudUrl:
-        "https://soundcloud.com/lucas-wd/jorge-e-mateus-a-hora-e-agora,https://soundcloud.com/creitu-silva-7/jorge-e-mateus-os-anjos-cantam-nosso-amor",
+        "https://soundcloud.com/tassiano-rezende-rodrigues-da-cunha/01-a-hora-agora,https://soundcloud.com/creitu-silva-7/jorge-e-mateus-os-anjos-cantam-nosso-amor",
       // Music title to display
       title: "A Hora é Agora, Os Anjos Cantam Nosso Amor",
       // Whether music plays automatically when website opens
